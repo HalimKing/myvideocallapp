91d9cdef3626506a12766db4a83d01ac487dcbcd
HalimKing <mohammedhalim1779@gmail.com>
2024-12-30 14:00:18 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import VideoCallApp from './components/VideoCallApp'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8">
+      <VideoCallApp />
+    </div>
+  )
+}
+
+export default App
